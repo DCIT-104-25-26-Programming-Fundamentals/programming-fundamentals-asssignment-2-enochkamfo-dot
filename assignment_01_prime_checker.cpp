41ad3e00861643ea4128b8010ff7a45ccaf9759d
enochkamfo-dot <enochkamfo@gmail.com>
2026-07-29 21:24:14 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -38,13 +38,10 @@
 using namespace std;
 
 bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n <= 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
+    if (n < 2) return false;
 
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
     }
     return true;
 }
@@ -57,8 +54,7 @@ int main() {
     if (isPrime(num))
         cout << num << " is a prime number." << endl;
     else
-        cout << num << " is not a prime number." << endl;
+        cout << num << " is NOT a prime number." << endl;
 
     return 0;
 }
-
